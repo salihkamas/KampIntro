f77542e4555bf83da94a520073c31e866e7cb18c
Salih Kamaş <msalihkamas@gmail.com>
2021-01-21 14:56:52 +0300

    20Ocak

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName+" güncellendi.");
+        }
+
+        public int Topla(int num1 , int num2)
+        {
+            return num1 + num2;
+        }
+        public void Topla2(int num1, int num2)
+        {
+            Console.WriteLine(num1+num2);
+        }
+    }
+}
